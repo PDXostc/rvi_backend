deb3abe22b0fbd8050e97e683a285537a68b7969
David Thiriez <dvthiriez@gmail.com>
2015-08-20 23:10:12 -0700

    attempting pull request merge
    keys.html updates

--- web/devices/tasks.py
+++ web/devices/tasks.py
@@ -17,7 +17,10 @@ from urlparse import urlparse
 
 from django.conf import settings
 
-import devices.models
+from django.contrib.auth.models import User, Group
+
+# import devices.models
+# from vehicles.models import Vehicle
 
 
 # Logging setup
@@ -35,7 +38,7 @@ def send_remote(remote):
     """
     
     logger.info('%s: Sending Remote.', remote)
-    
+
     global transaction_id
     
     # get settings
@@ -86,6 +89,15 @@ def send_remote(remote):
     # get destination info
     mobile = remote.rem_device
     dst_url = mobile.get_rvi_id()
+
+    # get user info
+    user = User.objects.get(id=mobile.account_id)
+    vehicle = remote.rem_vehicle
+
+    if vehicle.list_account() == user.username:
+        user_type = 'owner'
+    else:
+        user_type = 'guest'
  
     # notify remote of pending file transfer
     transaction_id += 1
@@ -96,6 +108,20 @@ def send_remote(remote):
                        timeout = int(time.time()) + 5000,
                        parameters = [{ u'certid': remote.rem_uuid },
                                      { u'certificate': cert },
+                                     {
+                                        u'username': user.username,
+                                        u'usertype': user_type,
+                                        u'vehicle': vehicle.veh_name,
+                                        u'services': {
+                                            u'lock': remote.rem_lock,
+                                            u'engine': remote.rem_engine,
+                                            u'trunk': remote.rem_trunk,
+                                            u'windows': remote.rem_windows,
+                                            u'lights': remote.rem_lights,
+                                            u'hazard': remote.rem_hazard,
+                                            u'horn': remote.rem_horn
+                                        }
+                                     },
                                     ])
     except Exception as e:
         logger.error('%s: Cannot connect to RVI service edge: %s', remote, e)
@@ -104,5 +130,102 @@ def send_remote(remote):
     logger.info('%s: Sent Certificate.', remote)
     return True
 
+def create_remote (remote):
+    """
+    Notify destination, typically a vehicle, of a pending software
+    update.
+    :param retry: sota.models.Retry object
+    """
 
+    logger.info('%s: Sending Remote.', remote)
 
+    global transaction_id
+
+    # get settings
+    # service edge url
+    try:
+        rvi_service_url = settings.RVI_SERVICE_EDGE_URL
+    except NameError:
+        logger.error('%s: RVI_SERVICE_EDGE_URL not defined. Check settings!', remote)
+        return False
+
+    # DM service id
+    try:
+        rvi_service_id = settings.RVI_DM_SERVICE_ID
+    except NameError:
+        rvi_service_id = '/dm'
+
+    # Signature algorithm
+    try:
+        alg = settings.RVI_BACKEND_ALG_SIG
+    except NameError:
+        alg = 'RS256'
+
+    # Server Key
+    try:
+        keyfile = open(settings.RVI_BACKEND_KEYFILE, 'r')
+        key = keyfile.read()
+    except Exception as e:
+        logger.error('%s: Cannot read server key: %s', remote, e)
+        return False
+
+    # Create and sign certificate
+    try:
+        cert = remote.encode_jwt(key, alg)
+    except Exception as e:
+        logger.error('%s: Cannot create and sign certificate: %s', remote, e)
+        return False
+
+    # establish outgoing RVI service edge connection
+    rvi_server = None
+    logger.info('%s: Establishing RVI service edge connection: %s', remote, rvi_service_url)
+    try:
+        rvi_server = jsonrpclib.Server(rvi_service_url)
+    except Exception as e:
+        logger.error('%s: Cannot connect to RVI service edge: %s', remote, e)
+        return False
+    logger.info('%s: Established connection to RVI Service Edge: %s', remote, rvi_server)
+
+    # get destination info
+    mobile = remote.rem_device
+    dst_url = mobile.get_rvi_id()
+
+    # get user info
+    user = User.objects.get(id=mobile.account_id)
+    vehicle = remote.rem_vehicle
+
+    if vehicle.list_account() == user.username:
+        user_type = 'owner'
+    else:
+        user_type = 'guest'
+
+    # notify remote of pending file transfer
+    transaction_id += 1
+    try:
+        rvi_server.message(calling_service = rvi_service_id,
+                       service_name = dst_url + rvi_service_id + '/cert_provision',
+                       transaction_id = str(transaction_id),
+                       timeout = int(time.time()) + 5000,
+                       parameters = [{ u'certid': remote.rem_uuid },
+                                     { u'certificate': cert },
+                                     {
+                                        u'username': user.username,
+                                        u'usertype': user_type,
+                                        u'vehicle': vehicle.veh_name,
+                                        u'services': {
+                                            u'lock': remote.rem_lock,
+                                            u'engine': remote.rem_engine,
+                                            u'trunk': remote.rem_trunk,
+                                            u'windows': remote.rem_windows,
+                                            u'lights': remote.rem_lights,
+                                            u'hazard': remote.rem_hazard,
+                                            u'horn': remote.rem_horn
+                                        }
+                                     },
+                                    ])
+    except Exception as e:
+        logger.error('%s: Cannot connect to RVI service edge: %s', remote, e)
+        return False
+
+    logger.info('%s: Sent Certificate.', remote)
+    return True
